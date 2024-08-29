6b80f70f2cb7098e86747d5c5146b15f594c9d04
ShieruCHR <81741561+ShieruCHR@users.noreply.github.com>
2024-08-29 14:39:11 +0900

    Initial Commit

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World from Backend!"}
+
+
+if __name__ == "__main__":
+    import asyncio
+    import uvicorn
+
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(uvicorn.run(app))
---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
+let backendProcess;
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.whenReady().then(() => {
+  startBackend();
+  createWindow();
+});
+app.on("closed", () => {
+  if (backendProcess) {
+    backendProcess.kill();
+  }
+});
+
+function startBackend() {
+  const backendExecutablePath = path.resolve(process.resourcesPath, "backend");
+  console.log(backendExecutablePath);
+  backendProcess = spawn(backendExecutablePath, [], {
+    cwd: path.dirname(backendExecutablePath),
+    shell: true,
+  });
+
+  backendProcess.stdout.on("data", (data) => {
+    console.log(`[Backend] ${data}`);
+  });
+}
